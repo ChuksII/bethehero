95b4b7564fd4ae6c5dcd4c5d5339085e25566919
ChuksII <ciloegbunam@gmail.com>
2020-05-02 02:39:36 -0300

    backend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (request, response) => {
+
+
+    return response.json({
+        mensagem: "hello world",
+    });
+});
+
+app.listen(3333);
